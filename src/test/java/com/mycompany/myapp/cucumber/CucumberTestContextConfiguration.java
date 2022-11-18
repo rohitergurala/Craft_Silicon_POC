4408968e36e2c2ccdda9d951202be1254fcf6479
Rohit Ergurala <Rohit.Ergurala@idctechnologies.com>
2022-11-18 10:45:39 +0400

    Initial Commit

---
+++ src/test/java/com/mycompany/myapp/cucumber/CucumberTestContextConfiguration.java
@@ -0,0 +1,11 @@
+package com.mycompany.myapp.cucumber;
+
+import com.mycompany.myapp.IntegrationTest;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@CucumberContextConfiguration
+@IntegrationTest
+@WebAppConfiguration
+public class CucumberTestContextConfiguration {
+}
